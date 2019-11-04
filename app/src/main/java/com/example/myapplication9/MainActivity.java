526d9b73c9ed006148dacf00facba22cc59e1cd0
liubohan1999 <723652665@qq.com>
2019-11-04 22:14:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication9;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_1);
+    }
+
+    public void onClick(View view) {
+        //控制逻辑代码
+        //Toast.makeText(this, "=", Toast.LENGTH_SHORT).show();
+        switch (view.getId())
+        {
+            case R.id.button_0:
+            case R.id.button_1:
+            case R.id.button_2:
+            case R.id.button_3:
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
