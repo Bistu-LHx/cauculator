7eda050c7a0cd1ee1e7f7927a2b5438e9568c792
liubohan1999 <723652665@qq.com>
2019-11-05 20:46:50 +0800

    DEC删除最后数 、清屏、括弧、等号功能完成，美化了一部分界面

--- app/src/main/java/com/example/myapplication9/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -7,6 +7,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.javia.arity.Symbols;
+import org.javia.arity.SyntaxException;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         //控制逻辑代码
         //Toast.makeText(this, "=", Toast.LENGTH_SHORT).show();
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.button_0:
             case R.id.button_1:
             case R.id.button_2:
@@ -40,30 +42,53 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button_div:
             case R.id.button_sin:
             case R.id.button_cos:
-            case R.id.button_tan:
-            {
+            case R.id.button_tan: {
                 Button btn = (Button) view;
                 String strAdded = btn.getText().toString();
-                TextView formula =(TextView) findViewById(R.id.formula_area);
+                TextView formula = (TextView) findViewById(R.id.formula_area);
                 String strContent = formula.getText().toString();
-                String strNewContent =strContent+strAdded;
+                String strNewContent = strContent + strAdded;
                 formula.setText(strNewContent);
             }
             break;
-            case R.id.button_equ:
-            {
-                TextView formula =(TextView) findViewById(R.id.formula_area);
+            case R.id.button_c: {
+                TextView formula = (TextView) findViewById(R.id.formula_area);
                 formula.setText("");
-                TextView result =(TextView) findViewById(R.id.result_area);
-                formula.setText("");
+                TextView result = (TextView) findViewById(R.id.result_area);
+                result.setText("");
             }
             break;
-            case R.id.button_c:{
+            case R.id.button_DEC: {
+                TextView formula = (TextView) findViewById(R.id.formula_area);
+                String StrContent = formula.getText().toString();
+                if (StrContent.length() > 0) {
+                    StrContent = StrContent.substring(0, StrContent.length() - 1);
+                    formula.setText(StrContent);
+                }
 
             }
             break;
-            case R.id.button_DEC:
-            {
+            case R.id.button_equ: {
+                TextView formula = (TextView) findViewById(R.id.formula_area);
+                String StrContent = formula.getText().toString();
+                try {
+                    Symbols s = new Symbols();
+                    double res = s.eval(StrContent);
+
+                    TextView result = (TextView) findViewById(R.id.result_area);
+                    result.setText(String.valueOf(res));
+                    formula.setText("");
+                } catch (SyntaxException e) {
+                    Toast.makeText(this, "Error!", Toast.LENGTH_LONG).show();
+
+                }
+
+                break;
+
+            }
+            case R.id.button_trans2:
+            case R.id.button_trans8:
+            case R.id.button_trans16: {
 
             }
             break;
